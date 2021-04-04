42ea3a492d501c41cb4f4574c8586229b0f420d6
Aleksandr Vinokruov <aleksandr.vinokuroff@mail.ru>
2021-04-04 21:55:43 +0300

    debug code for stats

--- TradingBot/common/Utils.cs
+++ TradingBot/common/Utils.cs
@@ -306,12 +306,11 @@ namespace TradingBot
         {
             var writer = new StreamWriter(outputFolder + "\\stat_" + DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss") + ".csv", false);
             writer.AutoFlush = true;
-            writer.WriteLine("Ticker;FileName;CountQuotes;PrevDayClose;TodayOpen;TodayClose;TodayMin;TodayMax;ChangeMinToClose;ChangeMinToOpen;ChangeOpenClose");
+            writer.WriteLine("Ticker;FileName;CountQuotes;ChangeOpenToLow;ChangeLowToClose");
 
             DirectoryInfo folder = new DirectoryInfo(candlesPath);
             foreach (FileInfo f in folder.GetFiles("*.csv", SearchOption.AllDirectories))
             {
-                // convert json quotes to simple csv data
                 var list = TradeBot.ReadCandles(f.FullName);
                 if (list.Count == 0)
                 {
@@ -324,57 +323,55 @@ namespace TradingBot
                 var fileName = Path.GetFileNameWithoutExtension(f.FullName) + "_" + figi + date;
                 var ticker = f.Name.Substring(0, f.Name.IndexOf("_"));
 
-                if (list.Count < 2)
+                if (list.Count < 1)
                     continue;
 
-                // check if we have previous day candle
-                int currDayStartPos = 0;
-                while (currDayStartPos < list.Count && list[currDayStartPos].Time.Hour > 20)
+                // skip 2 candles after market open
+                int i = 1;
+                int candlesSkipped = 0;
+                var prevCandle = list[0];
+                while (i < list.Count && candlesSkipped < 2)
                 {
-                    ++currDayStartPos; // skip all candles from previous day
-                }
+                    if (prevCandle.Time != list[i].Time)
+                    {
+                        ++candlesSkipped;
+                        prevCandle = list[i];
+                    }
 
-                if (currDayStartPos >= list.Count)
-                    continue;
-
-                decimal prevDayClose = 0.0m;
-                if (currDayStartPos > 0)
-                    prevDayClose = list[currDayStartPos - 1].Close; // else we just do not know close of previous day
-
-                var firstCandle = list[currDayStartPos];
-                decimal todayMin = firstCandle.Close;
-                decimal todayMax = firstCandle.Close;
-
-                int i = currDayStartPos;
-                while (i < list.Count && list[i].Time == firstCandle.Time)
-                {
-                    todayMin = Math.Min(todayMin, list[i].Close);
-                    todayMax = Math.Max(todayMax, list[i].Close);
                     ++i;
                 }
 
-                decimal volume = list[i - 1].Volume;
-                decimal todayClose = list[i - 1].Close;
-                int countQuotes = i - currDayStartPos;
-                var changeMinToClose = Helpers.GetChangeInPercent(todayMin, todayClose);
-                var changeMinToOpen = Helpers.GetChangeInPercent(todayMin, firstCandle.Close);
-                var changeOpenClose = Helpers.GetChangeInPercent(firstCandle.Close, todayClose);
-
-                if (volume > 100 && countQuotes > 10 && ((changeMinToClose > 1m && changeMinToOpen > 1m) || (changeOpenClose < -2m)))
+                while (i < list.Count)
                 {
-                    // so we have some metrics, lets output it to log file
-                    writer.WriteLine("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10}", ticker, fileName, countQuotes, prevDayClose, firstCandle.Close, todayClose, todayMin, todayMax, changeMinToClose, changeMinToOpen, changeOpenClose);
+                    // form-up full candle
+                    var startPos = i;
+                    while (i < list.Count && prevCandle.Time == list[i].Time)
+                    {
+                        prevCandle.Close = list[i].Close;
+                        prevCandle.High = Math.Max(prevCandle.High, list[i].Close);
+                        prevCandle.Low = Math.Min(prevCandle.Low, list[i].Close);
+                        ++i;
+                    }
 
-                    // copy file if output folder specified
-                    try
+                    // premarket and quotes count limit
+                    if (prevCandle.Time.Hour < 13 && i - startPos > 10)
                     {
-                        if (outputFolder.Length > 0)
-                            File.Copy(f.FullName, outputFolder + "\\" + f.Name);
+                        var changeOpenToLow = Helpers.GetChangeInPercent(prevCandle.Open, prevCandle.Low);
+                        var changeLowToClose = Helpers.GetChangeInPercent(prevCandle.Low, prevCandle.Close);
+
+                        if (changeOpenToLow <= -2m)
+                        {
+                            //Ticker; FileName; CountQuotes; ChangeOpenToClose; ChangeLowToClose
+                            writer.WriteLine("{0};{1};{2};{3};{4};{5}", ticker, fileName, prevCandle.Time.ToShortTimeString(), i - startPos, changeOpenToLow, changeLowToClose);
+                        }
                     }
-                    catch (Exception e)
+
+                    if (i < list.Count && prevCandle.Time != list[i].Time)
                     {
-                        Logger.Write("Exception happened while copying file. Error: " + e.Message);
+                        prevCandle = list[i];
                     }
+
+                    ++i;
                 }
             }
 
