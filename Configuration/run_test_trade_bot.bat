b62f47f7c8277bed36208a4b0719180f9b5dec56
Alexander Vinokurov <aleksandr.vinokuroff@mail.ru>
2021-06-08 00:39:30 +0300

    config change

--- Configuration/run_test_trade_bot.bat
+++ Configuration/run_test_trade_bot.bat
@@ -4,10 +4,10 @@ Setlocal EnableDelayedExpansion
 rem INPUT VARIABLES
 SET STRATEGY=MorningOpenStrategy
 SET INPUT_FOLDER=..\TestData\RawQuotes\5m\uncompressed\
-SET OUTPUT_FOLDER=
+SET OUTPUT_FOLDER=.\\test\\
 SET CPU_CORES=8
 
-
+echo test
 rem SCRIPT BODY
 SET PROCESSED=0
 FOR /f "tokens=*" %%G IN ('dir /b /s /a:d "%INPUT_FOLDER%"') DO (
@@ -20,4 +20,9 @@ FOR /f "tokens=*" %%G IN ('dir /b /s /a:d "%INPUT_FOLDER%"') DO (
 	) ELSE (
 		timeout /T 2 /NOBREAK)
 )
+
+IF %OUTPUT_FOLDER%=="" (
+		start ..\TradingBot\bin\Release\netcoreapp3.1\TradingBot.exe mode="AnalyzeLogs" Token="token.txt" ConfigPath="config.json" SourceFolder="%OUTPUT_FOLDER%"
+	) ELSE (
+
 
